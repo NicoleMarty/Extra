764ec1ee43927e77119a4c72c198ab3f97a7fbd1
Nicole Marty <nmarty310@gmail.com>
2019-11-07 14:27:56 -0600

    node server.js command returns text content of each news story card labeled under fashionweek page

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 // Dependencies
-var express = require("express");
-var mongojs = require("mongojs");
-// Require axios and cheerio. This makes the scraping possible
-var axios = require("axios");
 var cheerio = require("cheerio");
+var axios = require("axios");
 
-// Initialize Express
-var app = express();
+console.log("\n**************************\n" +
+    "Grabbing every thread name and link \n" +
+    "from reddit's webdev board:" +
+    "\n*******************\n");
 
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+axios.get("https://fashionista.com/fashion-week").then(function(response) {
+    var $ = cheerio.load(response.data);
+    var results = [];
+    $("div.m-card--content").each(function(i, element) {
+        var content = $(element).text();
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-    console.log("Database Error:", error);
-});
 
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-    res.send("Hello world");
-});
+        results.push({
+            content: content,
+
+        });
+    });
+    console.log(results);
+})
