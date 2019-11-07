fc6ada6874cb835713e3c36f8c53363319cc6dc8
Nicole Marty <nmarty310@gmail.com>
2019-11-07 16:11:13 -0600

    node server.js now returning link related to article information displayed in content:

--- server.js
+++ server.js
@@ -12,12 +12,15 @@ axios.get("https://fashionista.com/fashion-week").then(function(response) {
     var results = [];
     $("div.m-card--content").each(function(i, element) {
         var content = $(element).text();
+        $("phoenix-super-link").each(function(i, element) {
+            var link = $(element).attr("href");
 
-
-        results.push({
-            content: content,
-
+            results.push({
+                content: content,
+                link: link
+            });
         });
     });
+
     console.log(results);
 })
