5bb478d4376a7500acadc812baffb3d107f0c6b0
Nicole Marty <nmarty310@gmail.com>
2019-11-07 16:44:48 -0600

    added mongoose and rewrote server.js to scrape data to the mongodb db.

--- server.js
+++ server.js
@@ -1,26 +1,82 @@
 // Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Enable scraping
 var cheerio = require("cheerio");
 var axios = require("axios");
+
+// Initialize express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+    console.log("Database Error:", error);
+});
+
+// Main route (Hello Fabulous)
+app.get("/", function(req, res) {
+    res.send("Hello Fabulous");
+});
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+    // Find all results from the scrapedData collection in the db
+    db.scrapedData.find({}, function(error, found) {
+        // Throw any errors to console
+        if (error) {
+            console.log(error);
+        }
+        // If no errors, send data to browser as json
+        else {
+            res.json(found);
+        }
+    });
+});
 
 console.log("\n**************************\n" +
     "Grabbing every articles title, sub-title, meta data, and link \n" +
     "from fashionista.com Fashion Week section:" +
     "\n*******************\n");
 
-axios.get("https://fashionista.com/fashion-week").then(function(response) {
-    var $ = cheerio.load(response.data);
-    var results = [];
-    $("div.m-card--content").each(function(i, element) {
-        var content = $(element).text();
-        $("phoenix-super-link").each(function(i, element) {
-            var link = $(element).attr("href");
+// Scrape data from the site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+    axios.get("https://fashionista.com/fashion-week").then(function(response) {
+        var $ = cheerio.load(response.data);
+        var results = [];
+        $("div.m-card--content").each(function(i, element) {
+            var content = $(element).text();
+            $("phoenix-super-link").each(function(i, element) {
+                var link = $(element).attr("href");
+                // If the found elements have the content and link
+                if (content && link) {
+                    db.scrapedData.insert({
+                            content: content,
+                            link: link
+                        },
+                        function(error, inserted) {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                console.log(inserted);
+                            }
+                        })
+                }
 
-            results.push({
-                content: content,
-                link: link
             });
         });
     });
+    // Send a "Scrape Complete" message to the browser
+    res.send("Scrape Complete");
 
-    console.log(results);
-})
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
+});
