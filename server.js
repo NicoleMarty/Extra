56de9223fbd604db885ee61ef6a5ca24b1d8e9cf
Nicole Marty <nmarty310@gmail.com>
2019-11-11 15:58:35 -0600

    oops. forgot to install handlebars and update server.js accordingly. index file is now actually relevent

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
+require("dotenv").config();
 // Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 // Enable scraping
 var cheerio = require("cheerio");
 var axios = require("axios");
 
+//var db = require("./models");
+
 // Initialize express
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+//require("./routes/apiRoutes")(app);
+//require("./routes/htmlRoutes")(app);
+
+
 
 // Database configuration
 var databaseUrl = "scraper";
