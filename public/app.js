132492d0ead78213fa9bcc80154b4de6b8ca679e
Nicole Marty <nmarty310@gmail.com>
2019-11-11 16:38:25 -0600

    frontend displaying with correct data from the db

---
+++ public/app.js
@@ -0,0 +1,23 @@
+// We'll be rewriting the table's data frequently, so let's make our code more DRY
+// by writing a function that takes in 'animals' (JSON) and creates a table body
+function displayResults(scrapedData) {
+    // First, empty the table
+    $("tbody").empty();
+
+    // Then, for each entry of that json...
+    scrapedData.forEach(function(content) {
+        // Append each of the animal's properties to the table
+        var tr = $("<tr>").append(
+            $("<td>").text(content.content),
+            $("<td>").text(content.link)
+        );
+
+        $("tbody").append(tr);
+    });
+}
+
+// First thing: ask the back end for json with all animals
+$.getJSON("/all", function(data) {
+    // Call our function to generate a table body
+    displayResults(data);
+});
