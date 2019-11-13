55717ad2b2709c2191a7309fbfaef7e69522fc11
Nicole Marty <nmarty310@gmail.com>
2019-11-12 18:35:27 -0600

    latest updates. working on the comments/notes aspect. It doesn't look pretty, but it scrapes all the require info. Behind a bit due to illnesses.

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 // Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
@@ -11,33 +10,19 @@ var path = require("path");
 var cheerio = require("cheerio");
 var axios = require("axios");
 
-//var db = require("./models");
+// Require all models
+var db = require("./models");
 
+var PORT = 3000;
 
 // Initialize express
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Middleware
+// Configure Middleware
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-    "handlebars",
-    exphbs({
-        defaultLayout: "main"
-    })
-);
-app.set("view engine", "handlebars");
-
-// Routes
-//require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
-
-
 
 // Database configuration
 var databaseUrl = "scraper";
@@ -49,12 +34,47 @@ db.on("error", function(error) {
     console.log("Database Error:", error);
 });
 
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/scraped", { useNewUrlParser: true });
+
+// ROUTES
+// **********
+
+console.log("\n**************************\n" +
+    "Grabbing every articles title, sub-title, meta data, and link \n" +
+    "from fashionista.com Fashion Week section:" +
+    "\n*******************\n");
+
+// Scrape data from the site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+    axios.get("https://fashionista.com/fashion-week").then(function(response) {
+        var $ = cheerio.load(response.data);
+        $("div.l-grid--item").each(function(i, element) {
+            var content = $(element).text();
+            var link = $(element).children().attr("href");
+            // If the found elements have the content and link
+            if (content && link) {
+                db.Article.insert({
+                        content: content,
+                        link: "https://fashionista.com" + link,
+
+                    },
+                    function(error, inserted) {
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            console.log(inserted);
+                        }
+                    })
+            }
+        });
+        // Send a "Scrape Complete" message to the browser
+        res.send("Scrape Complete");
+    });
+});
 // Main route (Hello Fabulous)
 app.get("/", function(req, res) {
-    res.send("Hello Fabulous");
-    res.sendFile(path.join(__dirname + "./public/index.html"));
-
-
+    res.sendFile(path.join(__dirname + "./public/index.html"))
 });
 
 // Retrieve data from the db
@@ -71,39 +91,53 @@ app.get("/all", function(req, res) {
         }
     });
 });
-
-console.log("\n**************************\n" +
-    "Grabbing every articles title, sub-title, meta data, and link \n" +
-    "from fashionista.com Fashion Week section:" +
-    "\n*******************\n");
-
-// Scrape data from the site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-    axios.get("https://fashionista.com/fashion-week").then(function(response) {
-        var $ = cheerio.load(response.data);
-        $("div.l-grid--item").each(function(i, element) {
-            var content = $(element).text();
-            var link = $(element).children().attr("href");
-            // If the found elements have the content and link
-            if (content && link) {
-                db.scrapedData.insert({
-                        content: content,
-                        link: "https://fashionista.com" + link,
-
-                    },
-                    function(error, inserted) {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            console.log(inserted);
-                        }
-                    })
-            }
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function(dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
         });
-    });
-    // Send a "Scrape Complete" message to the browser
-    res.send("Scrape Complete");
+});
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate("note")
+        .then(function(dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function(dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
 });
 
 
