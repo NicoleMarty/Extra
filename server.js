4107ae687e0c65d035d9e729f1a1f6350bace7fd
Nicole Marty <nmarty310@gmail.com>
2019-11-13 16:15:41 -0600

    updating npm packages to make sure I have them all

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ db.on("error", function (error) {
 });
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scraped", { useNewUrlParser: true });
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongolab-clean-84739";
 
 
